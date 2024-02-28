e87b52682d89e01c561ce0de428cac5545519a7b
wlsql852 <108854643+wlsql852@users.noreply.github.com>
2024-02-28 15:06:10 +0900

    merge end

--- src/main/java/com/web/project/page/controller/ChampionsPage.java
+++ src/main/java/com/web/project/page/controller/ChampionsPage.java
@@ -1,8 +1,12 @@
 package com.web.project.page.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.web.project.dto.ChampionStatsDTO;
+import com.web.project.dto.TierDataDTO;
 import com.web.project.dto.info.Perk;
 import com.web.project.dto.info.Runes;
 import com.web.project.dto.info.Champion.Champion;
@@ -17,19 +23,61 @@ import com.web.project.dto.info.Champion.Spell;
 import com.web.project.dto.info.item.Item;
 import com.web.project.system.ChampionData;
 import com.web.project.system.ItemData;
+import com.web.project.system.JsonReader;
 import com.web.project.system.RuneData;
 import com.web.project.system.SummonerData;
 
 @Controller
 @RequestMapping("/yalolza.gg/champions")
 public class ChampionsPage {
+	private final JsonReader jsonReader;
 
+	@Autowired
+	public ChampionsPage(JsonReader jsonReader) {
+		this.jsonReader = jsonReader;
+	}
 	@GetMapping("")
-	public String Champions(Model model,
+	public String Champions(@RequestParam(required = false) String tier,
+			Model model,
 			@RequestParam(required = false) String position
 			) {
 		List<Champion> data = ChampionData.imagedata();
 		model.addAttribute("data", data);
+		try {
+	        if (tier != null && position != null) {
+	            TierDataDTO tierData = jsonReader.readJsonFile(tier);
+	            List<ChampionStatsDTO> allPositionData = new ArrayList<>();
+	            
+	            // 포지션 전체 ALL 추가
+	            if (position.equals("ALL")) {	                
+	                for (List<ChampionStatsDTO> positionData : tierData.getPositions().values()) {
+	                    allPositionData.addAll(positionData);
+	                }
+	            } else {
+	                allPositionData = tierData.getPositions().get(position);
+	            }
+
+	            // TierScore로 정렬
+	            allPositionData.sort((champ1, champ2) -> Double.compare(champ2.getStats().getTierScore(),
+	                    champ1.getStats().getTierScore()));
+
+	            // ALL 중복 챔피언 제거
+	            List<ChampionStatsDTO> uniqueChampions = new ArrayList<>();
+	            Set<String> addedChampions = new HashSet<>();
+	            for (ChampionStatsDTO champion : allPositionData) {
+	                if (!addedChampions.contains(champion.getChampionName())) {
+	                    uniqueChampions.add(champion);
+	                    addedChampions.add(champion.getChampionName());
+	                }
+	            }
+
+	            model.addAttribute("positionData", uniqueChampions);
+	        } else {
+	            model.addAttribute("error", "Invalid tier or position");
+	        }
+	    } catch (Exception e) {
+	        model.addAttribute("error", "ERROR: " + e.getMessage());
+	    }
 		return "champ";
 	}
 	@GetMapping("/")
