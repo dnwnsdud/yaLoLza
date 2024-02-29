6aa314c8aa86c83a6cc206c25319854aa67008f4
jinseong9887 <jinseong9887@gmail.com>
2024-02-29 16:33:58 +0900

    Revert "static,counter_detail,champ"
    This reverts commit b25ce5346b342edb7d2e78a00d7c079edc6712e0.

--- src/main/java/com/web/project/page/controller/CounterDataController.java
+++ src/main/java/com/web/project/page/controller/CounterDataController.java
@@ -1,6 +1,5 @@
 package com.web.project.page.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,117 +14,82 @@ import com.web.project.system.CounterJsonReader;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
+
 
 @Controller
 public class CounterDataController {
 
-	private final CounterJsonReader counterJsonReader;
+    private final CounterJsonReader counterJsonReader;
 
-	@Autowired
-	public CounterDataController(CounterJsonReader counterJsonReader) {
-		this.counterJsonReader = counterJsonReader;
-	}
+    @Autowired
+    public CounterDataController(CounterJsonReader counterJsonReader) {
+        this.counterJsonReader = counterJsonReader;
+    }
 
-	@GetMapping("/counter_detail/{position}/champion/{champion}")
-	public String getCounterData(@PathVariable String position, @PathVariable("champion") String champion,
-			@RequestParam(name = "champion", required = false) String additionalChampion, Model model) {
-		try {
-			CounterPositionDTO counterData = counterJsonReader.readCounterJsonFile();
-			List<CounterCountDTO> positionData = getPositionData(position, counterData);
+    @GetMapping("/counter/{position}/champion/{champion}")   //  http://localhost:9997/counter/top/champion/darius?champion=Aatrox
+    public String getCounterData(@PathVariable String position, 
+                                 @PathVariable("champion") String champion,
+                                 @RequestParam(name = "champion", required = false) String additionalChampion,
+                                 Model model) {
+        try {
+            CounterPositionDTO counterData = counterJsonReader.readCounterJsonFile();
+            List<CounterCountDTO> positionData = getPositionData(position, counterData);
 
-			if (positionData == null) {
-				return "error";
-			}
+            if (positionData == null) {
+                return "error";
+            }
 
-			List<CounterChampionDTO> targetCounters = new ArrayList<>();
-			List<CounterChampionDTO> otherChampions = new ArrayList<>();
-			Map<String, Integer> matchCounts = new HashMap<>();
+            List<CounterChampionDTO> targetCounters = new ArrayList<>();
+            for (CounterCountDTO countDTO : positionData) {
+                if (additionalChampion == null) {
+                    if (countDTO.getCounter().stream()
+                            .anyMatch(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion))) {
+                        targetCounters.addAll(countDTO.getCounter());
+                    }
+                } else {
+                    List<CounterChampionDTO> matchedCounters = countDTO.getCounter().stream()
+                        .filter(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion) ||
+                                                championDTO.getChampionName().equalsIgnoreCase(additionalChampion))
+                        .collect(Collectors.toList());
 
-			for (CounterCountDTO countDTO : positionData) {
-				if (additionalChampion == null) {
-					if (countDTO.getCounter().stream()
-							.anyMatch(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion))) {
-						targetCounters.addAll(countDTO.getCounter());
-					}
-				} else {
-					List<CounterChampionDTO> matchedCounters = countDTO.getCounter().stream()
-							.filter(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion)
-									|| championDTO.getChampionName().equalsIgnoreCase(additionalChampion))
-							.collect(Collectors.toList());
+                    boolean containsBothChampions = matchedCounters.stream()
+                        .anyMatch(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion)) &&
+                        matchedCounters.stream()
+                        .anyMatch(championDTO -> championDTO.getChampionName().equalsIgnoreCase(additionalChampion));
 
-					boolean containsBothChampions = matchedCounters.stream()
-							.anyMatch(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion))
-							&& matchedCounters.stream().anyMatch(
-									championDTO -> championDTO.getChampionName().equalsIgnoreCase(additionalChampion));
+                    if (containsBothChampions) {
+                        targetCounters.addAll(matchedCounters);
+                    }
+                }
+            }
+            
+    
+            model.addAttribute("selectedChampionName", champion);
+            model.addAttribute("targetCounters", targetCounters);
 
-					if (containsBothChampions) {
-						targetCounters.addAll(matchedCounters);
-						Collections.swap(targetCounters, 0,
-								targetCounters.indexOf(matchedCounters.stream()
-										.filter(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion))
-										.findFirst().orElse(null)));
-					}
-				}
+            return "counterView";
+        } catch (IOException e) {
+            model.addAttribute("error", "Data loading error: " + e.getMessage());
+            return "error";
+        }
+    }
 
-				if (!targetCounters.isEmpty()) {
-					CounterChampionDTO selectedChampion = targetCounters.stream()
-							.filter(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion)).findFirst()
-							.orElse(null);
-
-					if (selectedChampion != null) {
-						targetCounters.remove(selectedChampion);
-						targetCounters.add(0, selectedChampion);
-					}
-				}
-				for (CounterCountDTO countDTO1 : positionData) {
-				    // 선택된 챔피언(darius)이 포함된 CounterChampionDTO 객체만 찾기
-				    if (countDTO1.getCounter().stream().anyMatch(championDTO -> championDTO.getChampionName().equalsIgnoreCase(champion))) {
-				        for (CounterChampionDTO championDTO : countDTO1.getCounter()) {
-				            if (!championDTO.getChampionName().equalsIgnoreCase(champion) && !otherChampions.contains(championDTO)) {
-				                otherChampions.add(championDTO);
-				                matchCounts.put(championDTO.getChampionName(), countDTO1.getCount());
-				            }
-				        }
-				    }
-				}
-			}
-			
-			otherChampions.sort((champion1, champion2) -> Double.compare(champion2.getStats().getWinRate(), champion1.getStats().getWinRate()));
-
-		    model.addAttribute("selectedChampionName", champion);
-		    model.addAttribute("additionalChampionName", additionalChampion);
-		    model.addAttribute("targetCounters", targetCounters);
-		    model.addAttribute("otherChampions", otherChampions);
-		    model.addAttribute("matchCounts", matchCounts);
-
-			return "counter_detail";
-		} catch (IOException e) {
-			model.addAttribute("error", "Data loading error: " + e.getMessage());
-			return "error";
-		}
-	}
-	
-	
-
-	private List<CounterCountDTO> getPositionData(String position, CounterPositionDTO counterData) {
-		switch (position.toUpperCase()) {
-		case "TOP":
-			return counterData.getTop();
-		case "JUNGLE":
-			return counterData.getJungle();
-		case "MIDDLE":
-			return counterData.getMiddle();
-		case "BOTTOM":
-			return counterData.getBottom();
-		case "UTILITY":
-			return counterData.getUtility();
-		default:
-			return null;
-		}
-	}
+    private List<CounterCountDTO> getPositionData(String position, CounterPositionDTO counterData) {
+        switch (position.toUpperCase()) {
+            case "TOP":
+                return counterData.getTop();
+            case "JUNGLE":
+                return counterData.getJungle();
+            case "MIDDLE":
+                return counterData.getMiddle();
+            case "BOTTOM":
+                return counterData.getBottom();
+            case "UTILITY":
+                return counterData.getUtility();
+            default:
+                return null;
+        }
+    }
 }
