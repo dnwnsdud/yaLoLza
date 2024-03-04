28466d9cf7443078e80c572b22ba6568da3c8955
sinjawon <qlqhroal@naver.com>
2024-03-04 18:48:46 +0900

    2024-03-04
    티어이미지
    이미지적용

--- src/main/java/com/web/project/page/controller/DuoTestController.java
+++ src/main/java/com/web/project/page/controller/DuoTestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.web.project.function.Calall;
 
 
-
+ 
 @Controller
 @RequestMapping("/newlolkia22") 
 public class DuoTestController {
