961e65311256ce2fa9a1f9acba6e7ee710d0eb90
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2024-03-06 17:57:57 +0900

    2024-03-06
    비밀번호 찾기 안내 메시지 수정

--- src/main/java/com/web/project/api/controller/SendEmailService.java
+++ src/main/java/com/web/project/api/controller/SendEmailService.java
@@ -26,7 +26,9 @@ public class SendEmailService {
         MailDTO dto = new MailDTO();
         dto.setAddress(userEmail);
         dto.setTitle("야롤자 임시비밀번호 안내 이메일 입니다.");
-        dto.setMessage("안녕하세요. 야롤자 임시 비밀번호 안내 관련 이메일 입니다." + "회원님의 임시 비밀번호는 [ " + str + " ] 입니다." + "로그인 후에 비밀번호를 변경을 해주세요.");
+        dto.setMessage("안녕하세요. 야롤자 임시 비밀번호 안내 관련 이메일 입니다.\n"
+                + "회원님의 임시 비밀번호는 [ " + str + " ] 입니다.\n"
+                + "로그인 후에 비밀번호를 변경을 해주세요.");
         updatePassword(str, userEmail);
         return dto;
     }
