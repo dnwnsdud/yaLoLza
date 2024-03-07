a631d45554b0a6a314015278b52ce7dec0ef2a25
우준영 <143519025+dnwnsdud@users.noreply.github.com>
2024-03-07 18:51:31 +0900

    2024-03-07
    수정

--- src/main/java/com/web/project/page/controller/StaticController.java
+++ src/main/java/com/web/project/page/controller/StaticController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.web.project.dto.championstats.AramChampionDTO;
@@ -22,6 +23,7 @@ import java.util.List;
 import java.util.Set;
 
 @Controller
+@RequestMapping("/yalolza.gg/champions")
 public class StaticController {
 
     private final JsonReader jsonReader;
